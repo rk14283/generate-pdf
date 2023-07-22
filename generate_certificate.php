8998411db9d7de116213196f1e21b93786be82fc
rk14283 <rk14283@my.bristol.ac.uk>
2023-07-22 11:24:39 +0100

    generating certificate of completion

--- generate_certificate.php
+++ generate_certificate.php
@@ -1,14 +1,20 @@
 <?php
-
+//Including the TCPDF library 
  require_once('tcpdf.php');
+ //Retrieving data from the POST request 
 $name = $_POST["name"]; 
 $course = $_POST["course"]; 
 $date = $_POST["date"]; 
-
+//Creating a new TCPDF instance 
 $pdf = new TCPDF('L', 'mm', 'A4', true, 'UTF-8', true);
+//Setting the PDF title
 $pdf->setTitle('Cerificate of completion'); 
+//Adding a new page to the PDF document
 $pdf->AddPage(); 
+
+// Setting the font to 'helvetica' with a size of 24
 $pdf->SetFont('helvetica','',24);
+// Writing HTML content to the PDF
 $pdf->writeHTML(
     "
     <style>
@@ -22,5 +28,6 @@ $pdf->writeHTML(
     <h2> $course </h2>
     <p> on $date </p>
     ");
+    // Outputting the PDF as a download with a custom file name
     $pdf->Output($name.'-'.$course.'-'.'certificate.pdf','D'); 
 ?>
